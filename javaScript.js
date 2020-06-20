eb20ff3db560c8b882fc6e8741815803d1c05737
Murad Atta <murad.atta@gmail.com>
2020-06-20 19:04:26 -0400

    pushing the initial design and css file

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,20 @@
-console.log("starting application");
+
+
+
+// var clientDd = "TPKPQAPUEUIJXVRZE1LKTJ2PPNBEHUBAD3LKXDIOTXGGIKKZ"
+// var clientSecret = "3H45J3FEI2SRFIX5ONNIMED31NJGJATZRBSIHS5CIQZJV3KQ"
+// var queryURL = "https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=TPKPQAPUEUIJXVRZE1LKTJ2PPNBEHUBAD3LKXDIOTXGGIKKZ&client_secret=3H45J3FEI2SRFIX5ONNIMED31NJGJATZRBSIHS5CIQZJV3KQ&v=20200620";
+
+// console.log("hello")
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }) .then(function(response){
+//     console.log(response)
+// })
+
+
+$(document).ready(function(){
+    $('select').formSelect();
+    instance.destroy();
+  });
