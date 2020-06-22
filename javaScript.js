d473e9b1ab43dacec19258050132bb5a6adc41d0
Murad Atta <murad.atta@gmail.com>
2020-06-22 12:14:21 -0400

    saving changes

--- javaScript.js
+++ javaScript.js
@@ -1,20 +1,50 @@
 
 
 
-// var clientDd = "TPKPQAPUEUIJXVRZE1LKTJ2PPNBEHUBAD3LKXDIOTXGGIKKZ"
-// var clientSecret = "3H45J3FEI2SRFIX5ONNIMED31NJGJATZRBSIHS5CIQZJV3KQ"
-// var queryURL = "https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=TPKPQAPUEUIJXVRZE1LKTJ2PPNBEHUBAD3LKXDIOTXGGIKKZ&client_secret=3H45J3FEI2SRFIX5ONNIMED31NJGJATZRBSIHS5CIQZJV3KQ&v=20200620";
+ var clientId = "TPKPQAPUEUIJXVRZE1LKTJ2PPNBEHUBAD3LKXDIOTXGGIKKZ"
+ var clientSecret = "3H45J3FEI2SRFIX5ONNIMED31NJGJATZRBSIHS5CIQZJV3KQ"
+ var currentDate = moment().format("YYYYMMDD")
 
-// console.log("hello")
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }) .then(function(response){
-//     console.log(response)
-// })
+
+
+
+
 
 
 $(document).ready(function(){
     $('select').formSelect();
-    instance.destroy();
+   
+$("a").on("click", function (event) {
+
+    event.preventDefault();
+    var userInput = $("#city").val();
+    var queryURL = "https://api.foursquare.com/v2/venues/explore?" + "mode=url" + "&near=" + userInput + "&client_id=" + clientId + "&client_secret=" + clientSecret +"&v=20200621";
+    var weatherURL = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location=sunnyvale";
+
+    
+    console.log(userInput)
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }) .then(function(response){
+       // console.log(response)
+    });
+
+
+
+    $.ajax({
+        url: weatherURL,
+        method: "GET"
+    }) .then(function(response){
+        console.log(response)
+    });
+});
+
+
+
+
+
   });
+//Dallas, TX, USA
